3ebb8c6686affdfa208faf1cf09d7c1039722af9
Alexandre Kaimer <alexandre.kaimer@original.io>
2020-07-15 16:41:54 -0300

    add state modifier - by button

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -14,11 +14,18 @@ export default class Counter extends React.Component {
     number: this.props.initialNumber
   }
 
+  inc = () => {
+    this.setState({
+      number: this.state.number + 1
+    })
+  }
+
   render() {
     return (
       <div>
         <h2>Counter</h2>
         <p>Initial value: {this.state.number}</p>
+        <button onClick={this.inc}>+</button>
       </div>
     )
   }
