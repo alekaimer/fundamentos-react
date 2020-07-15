b306026dca43eb4979a60e64add13b60deb9f7bf
Alexandre Kaimer <alexandre.kaimer@original.io>
2020-07-15 14:08:31 -0300

    set state of teh component with useState

--- src/components/forms/Input.jsx
+++ src/components/forms/Input.jsx
@@ -2,12 +2,16 @@
 import React, { useState } from 'react'
 
 export default props => {
-
   const [value, setValue] = useState(`initial`)
-
+  function changeState(event){
+    setValue(event.target.value)
+    console.log(event.target.value)
+  }
+  
   return (
     <div>
-      <input value={value}/>
+      <h2>{value}</h2>
+      <input value={value} onChange={changeState}/>
     </div>
   )
 }
