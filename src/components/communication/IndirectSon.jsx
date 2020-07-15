8e3989198238ad294ab1815101b1e2424c157583
Alexandre Kaimer <alexandre.kaimer@original.io>
2020-07-15 13:04:59 -0300

    adiciona randomizador de resultado

--- src/components/communication/IndirectSon.jsx
+++ src/components/communication/IndirectSon.jsx
@@ -5,12 +5,13 @@ export default props => {
 
   // const callBackFunction = props.propWhenClick
   const ageGenerator = () => parseInt(Math.random() * (100 - 21)) + 21
+  const nerdGenerator = () => Math.random() > 0.5
 
   return (
     <div>
       Son component: 
       {/* <button onClick={_ => { callBackFunction('John', 35, true) }}>Send information!</button> */}
-      <button onClick={function(){props.propWhenClick('John', ageGenerator(), true)}}>Send information!</button>
+      <button onClick={function(){props.propWhenClick('John', ageGenerator(), nerdGenerator())}}>Send information!</button>
     </div>
   )
 }
